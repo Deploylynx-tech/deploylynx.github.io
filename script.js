b08a903783818f435942810bfd9f07f4a8ce5730
DeployLynx <deploylynx26@gmail.com>
2026-03-19 03:53:54 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
+/* CURSOR */
+const cursor = document.createElement("div");
+cursor.classList.add("cursor");
+document.body.appendChild(cursor);
 
+document.addEventListener("mousemove", e=>{
+cursor.style.left = e.pageX + "px";
+cursor.style.top = e.pageY + "px";
+});
+
+/* THEME */
+function toggleTheme(){
+document.body.classList.toggle("light");
+}
+
+/* EMAIL FORM */
+function sendMail(e){
+e.preventDefault();
+
+let name=document.getElementById("name").value;
+let email=document.getElementById("email").value;
+let message=document.getElementById("message").value;
+
+window.location.href=`mailto:deploylynx26@gmail.com?subject=Client from ${name}&body=${message} (${email})`;
+}
